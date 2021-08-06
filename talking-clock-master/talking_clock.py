cc8445e6af9799cf05ce7a09c05436e72c2d134d
saurabh nayak <68698234+saurabh0418@users.noreply.github.com>
2021-08-06 23:26:18 +0530

    talking_clock_uploads

---
+++ talking-clock-master/talking_clock.py
@@ -0,0 +1,143 @@
+import os
+import pyaudio
+import wave
+import datetime
+
+
+class TalkingClock(object):
+
+    number_to_word = {
+        0: " twelve",
+        1: " one",
+        2: " two",
+        3: " three",
+        4: " four",
+        5: " five",
+        6: " six",
+        7: " seven",
+        8: " eight",
+        9: " nine",
+        10: " ten",
+        11: " eleven",
+        12: " twelve",
+        13: " thirteen",
+        14: " fourteen",
+        15: " fifteen",
+        16: " sixteen",
+        17: " seventeen",
+        18: " eighteen",
+        19: " nineteen",
+        20: " twenty",
+        30: " thirty",
+        40: " fourty",
+        50: " fifty",
+    }
+
+    tens_to_word = {
+        2: " twenty",
+        3: " thirty",
+        4: " fourty",
+        5: " fifty",
+    }
+
+    def time_string_to_words(self, time):
+        """
+        Time String to Words
+        --------------------
+        Convert time string in format HH:MM to its english
+        speaking equivalent
+        """
+        try:
+            hour, minutes = [int(x) for x in time.split(":")]
+        except ValueError:
+            raise ValueError("Time not in format 'HH:MM'")
+
+        output = "It's"
+        output += self.number_to_word[hour % 12]
+
+        period = " am" if (hour % 24 < 12) else " pm"
+        minutes = minutes % 60
+
+        if minutes == 0:
+          "pass"
+        elif minutes < 20:
+            output += (" oh" if minutes < 10 else "") + self.number_to_word[minutes]
+        else:
+            tens, ones = divmod(minutes, 10)
+            output += self.tens_to_word[tens]
+
+            if minutes % 10:
+                output += self.number_to_word[ones]
+
+        return output + period
+
+    def time_to_speech(self, sentance):
+        """
+        Time to Speech
+        --------------
+        If possible, queue sound files according to sentance.
+        Then, say the sentance out loud (uses speakers).
+        """
+        # Invert: Keys become values, values become keys
+        word_to_number = {v[1:]: str(k) for k, v in self.number_to_word.items()}
+
+        directory = os.path.dirname(os.path.realpath(__file__))
+
+        sentance = sentance.split()
+
+        sounds_to_play = ["HOUR1", "its"]
+        am_or_pm = sentance[-1]
+        above_12 = False
+
+        sounds_to_play.append(word_to_number[sentance[1]])  # It's 'nine' thirty
+
+        for i in ["ty", "teen"]:
+            if sentance[2].endswith(i):  # It's ten ['thirty', 'thirteen', ... ]
+                sounds_to_play.append(sentance[2].split(i)[0])
+                sounds_to_play.append(i)
+                above_12 = True
+
+        if len(sentance) == 4 and not above_12:  # It's five ['ten', 'eleven', 'twelve']
+            sounds_to_play.append(word_to_number[sentance[2]])
+
+        if len(sentance) == 5:
+            if sentance[2] == "oh":  # ex. It's nine 'oh' five am
+                sounds_to_play.append("o")
+            sounds_to_play.append(word_to_number[sentance[3]])  # It's nine twenty 'five' am
+
+        sounds_to_play.append(am_or_pm)
+        sounds_to_play.append("hour2")
+
+        for i in sounds_to_play:
+            self.play_sounds(os.path.join(directory, "sound_clips", i + ".wav"))
+
+    @staticmethod
+    def play_sounds(sound_path):
+        chunk = 1024
+        f = wave.open(sound_path, "rb")
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                        channels=f.getnchannels(),
+                        rate = f.getframerate(),
+                        output=True)
+
+        data = f.readframes(chunk)
+
+        while data:
+            stream.write(data)
+            data = f.readframes(chunk)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+
+if __name__ == '__main__':
+    Talker = TalkingClock()
+
+    time = datetime.datetime.now().strftime("%H:%M")
+
+    sentance = Talker.time_string_to_words(time)
+    Talker.time_to_speech(sentance)
+
---
+++ talking-clock-master/test_talking_clock.py
@@ -0,0 +1,76 @@
+from unittest import TestCase
+from talking_clock import TalkingClock
+
+class TestTalkingClock(TestCase):
+    def setUp(self):
+        self.Talker = TalkingClock()
+
+    def tearDown(self):
+        del self.Talker
+
+    def test_all_zeros(self):
+        self.assertEqual(self.Talker.time_string_to_words("00:00"), "It's twelve am")
+
+    def test_single_digit_minutes(self):
+        test_cases = [
+                ("1", "one"), ("2", "two"), ("3", "three"), ("4", "four"), ("5", "five"),
+                    ("6", "six"), ("7", "seven"), ("8", "eight"), ("9", "nine"),
+            ]
+        for i in test_cases:
+            result = self.Talker.time_string_to_words("00:0{}".format(i[0]))
+            expected_result = "It's twelve oh {} am".format(i[1])
+            self.assertEqual(result, expected_result)
+
+    def test_tens_plus_single_digits(self):
+        test_cases = [
+                ("1", "one"), ("2", "two"), ("3", "three"), ("4", "four"), ("5", "five"),
+                ("6", "six"), ("7", "seven"), ("8", "eight"), ("9", "nine"),
+            ]
+        for i in test_cases:
+            result = self.Talker.time_string_to_words("00:2{}".format(i[0]))
+            expected_result = "It's twelve twenty {} am".format(i[1])
+            self.assertEqual(result, expected_result)
+
+    def test_all_minute_tens(self):
+        test_cases = [("20", "twenty"), ("30", "thirty"), ("40", "fourty"), ("50", "fifty")]
+        for i in test_cases:
+            result = self.Talker.time_string_to_words("00:{}".format(i[0]))
+            expected_result = "It's twelve {} am".format(i[1])
+            self.assertEqual(result, expected_result)
+
+    def test_over_sixty_minutes(self):
+        self.assertEqual(self.Talker.time_string_to_words("00:65"), "It's twelve oh five am")
+
+    def test_over_twenty_four_hours(self):
+        self.assertEqual(self.Talker.time_string_to_words("26:00"), "It's two am")
+
+    def test_negative_hours(self):
+        self.assertEqual(self.Talker.time_string_to_words("-2:00"), "It's ten pm")
+
+    def test_negative_minutes(self):
+        self.assertEqual(self.Talker.time_string_to_words("00:-10"), "It's twelve fifty am")
+
+    def test_not_a_time(self):
+        self.assertRaisesRegexp(ValueError, "Time not in format",
+                                self.Talker.time_string_to_words, "blaaa, cheese 23984742")
+
+    def test_numbers_with_no_colon(self):
+        self.assertRaisesRegexp(ValueError, "Time not in format",
+                                self.Talker.time_string_to_words, "0259")
+
+    def test_time_with_seconds(self):
+        self.assertRaisesRegexp(ValueError, "Time not in format",
+                                self.Talker.time_string_to_words, "02:59:02")
+
+    def test_reddit_sample_data(self):
+        test_cases = [
+                ("00:00", "It's twelve am"),
+                    ("01:30", "It's one thirty am"),
+                    ("12:05", "It's twelve oh five pm"),
+                    ("14:01", "It's two oh one pm"),
+                    ("20:29", "It's eight twenty nine pm"),
+                    ("21:00", "It's nine pm"),
+            ]
+        for i in test_cases:
+            self.assertEqual(self.Talker.time_string_to_words(i[0]), i[1])
+
